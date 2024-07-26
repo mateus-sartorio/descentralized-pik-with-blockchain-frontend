3963e09c7e5ae868ee4e1756fe3856c11ddb87ef
mateus-sartorio <mateussartorio39@gmail.com>
2024-07-25 22:56:46 -0300

    unecessary types of input removed from Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,3 @@
-// Copyright 2022 Cartesi Pte. Ltd.
-
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy
-// of the license at http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-
 import { FC } from "react";
 import injectedModule from "@web3-onboard/injected-wallets";
 import { init } from "@web3-onboard/react";
@@ -41,7 +29,7 @@ init({
 });
 
 const App: FC = () => {
-  const [dappAddress, setDappAddress] = useState<string>("0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C");
+  const [dappAddress, setDappAddress] = useState<string>("0xab7528bb862fb57e8a2bcd567a2e929a0be56a5e");
 
   return (
     <div>
@@ -53,18 +41,24 @@ const App: FC = () => {
             value={dappAddress}
             onChange={(e) => setDappAddress(e.target.value)}
           />
-          <br /><br />
+          <br />
+          <br />
         </div>
-        <h2>Inspect</h2>
-        <Inspect />
+        
+        {/* <h2>Inspect</h2>
+        <Inspect /> */}
+        
         <h2>Input</h2>
         <Input dappAddress={dappAddress} />
-        <h2>Reports</h2>
-        <Reports />
-        <h2>Notices</h2>
-        <Notices />
-        <h2>Vouchers</h2>
-        <Vouchers dappAddress={dappAddress} />
+        
+        {/* <h2>Reports</h2>
+        <Reports /> */}
+        
+        {/* <h2>Notices</h2>
+        <Notices /> */}
+        
+        {/* <h2>Vouchers</h2>
+        <Vouchers dappAddress={dappAddress} /> */}
       </GraphQLProvider>
     </div>
   );
