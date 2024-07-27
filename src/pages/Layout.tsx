3460f9832d8fa4f1c7c70343a64b50bf77a24c6a
mateus-sartorio <mateussartorio39@gmail.com>
2024-07-27 20:20:32 -0300

    Navbar and sidebar updates added to Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Sidebar from '../components/Sidebar/Sidebar';
+import Footer from '../components/Footer/Footer';
 
 interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-  <div className="min-h-screen bg-gray-100 flex">
-    <Sidebar/>
-      <div className="flex-1">
-        <Navbar/>
-        <div className="p-4">
-          <Outlet />
+    <>
+      <div className="min-h-screen bg-gray-100 flex">
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+          
+        <div className="flex-1">
+          <Navbar toggleSidebar={toggleSidebar} />
+          <div className="p-4 mt-16">
+            <Outlet />
+          </div>
         </div>
       </div>
-  </div>
+
+      <Footer/>
+    </>
   );
 };
 
